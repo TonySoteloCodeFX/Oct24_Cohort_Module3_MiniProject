26a092568e0a875959904e8a5df55602429ebd91
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-10-21 01:28:54 -0700

    Mini Project Complete

---
+++ README.md
@@ -0,0 +1,39 @@
+<h3><u>Steps</u></h3>
+
+1. Run contact management system python file
+2. Select an option
+
+<h3><u>Functions</u></h3>
+
+<div><i><b>add_contact()</b></i></div>
+
+ - Adds contact to contact dictionary & returns confirmation.
+
+<div><i><b>edit_contact()</b></i></div>
+
+ - Deletes given contact and creates a new one.
+
+<div><i><b>delete_contact()</b></i></div>
+
+ - Deletes given contact from contact dictionary and prints confirmation.
+
+<div><i><b>search_contact()</b></i></div>
+
+ - Searches contact dictionary and returns given number's contact details.
+
+<div><i><b>display_contacts()</b></i></div>
+
+ - Displays all contacts in dictionary.
+
+<div><i><b>export_contacts()</b></i></div>
+
+ - Exports all contacts to a txt file.
+
+<div><i><b>import_contacts()</b></i></div>
+
+ - Takes text file and adds contacts to contact dictionary.
+
+<div><i><b>run_program()</b></i></div>
+
+ - Runs Menu Options. Serves as the Main Program.
+
--- contact_management_system.py
+++ contact_management_system.py
@@ -25,7 +25,7 @@ def add_contact():
 
             contact_number = input(f"{Fore.GREEN}Enter phone number: ex: xxx-xxx-xxxx{Style.RESET_ALL}\n")
             print("-" * 50)
-            if not re.match(number_pattern, contact_number):
+            if not re.match(number_pattern, contact_number) or contact_number in contact_dict:
                 clr()
                 print("-" * 50)
                 print(f"{Fore.RED}Invalid Number.{Style.RESET_ALL} Try again. ex: xxx-xxx-xxxx")
@@ -75,152 +75,224 @@ def add_contact():
             print(f"{Fore.RED}Error:{Style.RESET_ALL} {e}")
             
 def edit_contact():
-    '''deletes given contact and creates a new one.'''
+    '''Deletes given contact and creates a new one.'''
     clr()
     print("-" * 50)
     keep_running = True
     while keep_running:
         try:
-            print(f"{Fore.GREEN}Enter number to edit for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
-            contact_edit = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
-            print("-" * 50)
-
-            if contact_edit in contact_dict:
+            if len(contact_dict) == 0:
                 clr()
                 print("-" * 50)
-                del contact_dict[contact_edit]
+                print(f"{Fore.GREEN}There's no contacts to edit.{Style.RESET_ALL}")
+                print("-" * 50)
+                keep_running = False
+            else:
+                print(f"{Fore.GREEN}Enter number to edit for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
+                contact_edit = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
+                print("-" * 50)
 
-            correct_number = input(f"{Fore.GREEN}Enter correct phone number: ex: xxx-xxx-xxxx{Style.RESET_ALL}\n")
-            print("-" * 50)
-            if not re.match(number_pattern, correct_number):
+                if contact_edit in contact_dict:
+                    clr()
+                    print("-" * 50)
+                    del contact_dict[contact_edit]
+
+                correct_number = input(f"{Fore.GREEN}Enter correct phone number: ex: xxx-xxx-xxxx{Style.RESET_ALL}\n")
+                print("-" * 50)
+                if not re.match(number_pattern, correct_number):
+                    clr()
+                    print("-" * 50)
+                    print(f"{Fore.RED}Invalid Number.{Style.RESET_ALL} Try again. ex: xxx-xxx-xxxx")
+                    print("-" * 50)
+                    continue
+
+                correct_name = input(f"{Fore.GREEN}Enter correct full name: ex: John Doe{Style.RESET_ALL}\n")
+                print("-" * 50)
+                if not re.match(name_pattern, correct_name):
+                    clr()
+                    print("-" * 50)
+                    print(f"{Fore.RED}Invalid Name.{Style.RESET_ALL} Try again. ex: John Doe")
+                    print("-" * 50)
+                    continue
+
+                correct_email = input(f"{Fore.GREEN}Enter correct email: ex: john_doe@gmail.com{Style.RESET_ALL}\n")
+                print("-" * 50)
+                if not re.match(email_pattern, correct_email):
+                    clr()
+                    print("-" * 50)
+                    print(f"{Fore.RED}Invalid Email.{Style.RESET_ALL} Try again. ex: john_doe@gmail.com")
+                    print("-" * 50)
+                    continue
+
+                correct_title = input(f"{Fore.GREEN}Personal or Professional:{Style.RESET_ALL}\n")
+                print("-" * 50)
+                correct_notes = input(f"{Fore.GREEN}Enter Notes:{Style.RESET_ALL}\n")
+
+                contact_dict[correct_number] = {
+                    "Name": correct_name,
+                    "Title": correct_title,
+                    "Email":correct_email,
+                    "Notes": correct_notes
+                }
+
                 clr()
                 print("-" * 50)
-                print(f"{Fore.RED}Invalid Number.{Style.RESET_ALL} Try again. ex: xxx-xxx-xxxx")
+                print(f"{Fore.GREEN}The following contact was edited.{Style.RESET_ALL}")
                 print("-" * 50)
-                continue
-
-            correct_name = input(f"{Fore.GREEN}Enter correct full name: ex: John Doe{Style.RESET_ALL}\n")
-            print("-" * 50)
-            if not re.match(name_pattern, correct_name):
-                clr()
+                print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {correct_title}\n{Fore.GREEN}Name:{Style.RESET_ALL} {correct_name}\n{Fore.GREEN}Phone Number:{Style.RESET_ALL}{correct_number}\n{Fore.GREEN}Email:{Style.RESET_ALL} {correct_email}\n{Fore.GREEN}Notes:{Style.RESET_ALL} {correct_notes}")
                 print("-" * 50)
-                print(f"{Fore.RED}Invalid Name.{Style.RESET_ALL} Try again. ex: John Doe")
-                print("-" * 50)
-                continue
 
-            correct_email = input(f"{Fore.GREEN}Enter correct email: ex: john_doe@gmail.com{Style.RESET_ALL}\n")
-            print("-" * 50)
-            if not re.match(email_pattern, correct_email):
-                clr()
-                print("-" * 50)
-                print(f"{Fore.RED}Invalid Email.{Style.RESET_ALL} Try again. ex: john_doe@gmail.com")
-                print("-" * 50)
-                continue
-
-            correct_title = input(f"{Fore.GREEN}Personal or Professional:{Style.RESET_ALL}\n")
-            print("-" * 50)
-            correct_notes = input(f"{Fore.GREEN}Enter Notes:{Style.RESET_ALL}\n")
-
-            contact_dict[correct_number] = {
-                "Name": correct_name,
-                "Title": correct_title,
-                "Email":correct_email,
-                "Notes": correct_notes
-            }
-
-            clr()
-            print("-" * 50)
-            print(f"{Fore.GREEN}The following contact was edited.{Style.RESET_ALL}")
-            print("-" * 50)
-            print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {correct_title}\n{Fore.GREEN}Name:{Style.RESET_ALL} {correct_name}\n{Fore.GREEN}Phone Number:{Style.RESET_ALL}{correct_number}\n{Fore.GREEN}Email:{Style.RESET_ALL} {correct_email}\n{Fore.GREEN}Notes:{Style.RESET_ALL} {correct_notes}")
-            print("-" * 50)
-
-            keep_running = False
+                keep_running = False
         except Exception as e:
             clr()
             print("-" * 50)
             print(f"{Fore.RED}Error:{Style.RESET_ALL} {e}")
+            print("-" * 50)
 
 def delete_contact():
-    '''Deletes given number contact from contact dictionary and prints confirmation.'''
-    try:
+    '''Deletes given contact from contact dictionary and prints confirmation.'''
+    if len(contact_dict) == 0:
         clr()
         print("-" * 50)
-        print(f"{Fore.GREEN}Enter number to delete for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
-        contact_delete = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
-
-        if contact_delete in contact_dict:
-            clr()
-            print("-" * 50)
-            contact_details = contact_dict[contact_delete]
-            print(f"{Fore.GREEN}The following contact has been deleted.{Style.RESET_ALL}")
-            print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {contact_details['Title']}")
-            print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
-            print(f"{Fore.GREEN}Phone Number: {Style.RESET_ALL}{contact_delete}")
-            print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
-            print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
-            del contact_dict[contact_delete]
-            print("-" * 50)
-        else:
-            clr()
-            print("-" * 50)
-            print(f"{Fore.GREEN}The number:{Style.RESET_ALL} {contact_delete}. {Fore.GREEN}Does not exist. Try again.{Style.RESET_ALL}")
-
-    except ValueError:
-        clr()
+        print(f"{Fore.GREEN}There's no contacts to delete.{Style.RESET_ALL}")
         print("-" * 50)
-        print(f"{Fore.RED}Error:{Style.RESET_ALL} Incorrect input")
+    else:    
+        try:
+            clr()
+            print("-" * 50)
+            print(f"{Fore.GREEN}Enter number to delete for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
+            contact_delete = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
+
+            if contact_delete in contact_dict:
+                clr()
+                print("-" * 50)
+                contact_details = contact_dict[contact_delete]
+                print(f"{Fore.GREEN}The following contact has been deleted.{Style.RESET_ALL}")
+                print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {contact_details['Title']}")
+                print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
+                print(f"{Fore.GREEN}Phone Number: {Style.RESET_ALL}{contact_delete}")
+                print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
+                print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
+                del contact_dict[contact_delete]
+                print("-" * 50)
+            else:
+                clr()
+                print("-" * 50)
+                print(f"{Fore.GREEN}The number:{Style.RESET_ALL} {contact_delete}. {Fore.GREEN}Does not exist. Try again.{Style.RESET_ALL}")
+                print("-" * 50)
+
+        except ValueError:
+            clr()
+            print("-" * 50)
+            print(f"{Fore.RED}Error:{Style.RESET_ALL} Incorrect input")
 
 def search_contact():
     '''Searches contact dictionary and returns given number's contact details.'''
-    try:
+    if len(contact_dict) == 0:
         clr()
         print("-" * 50)
-        print(f"{Fore.GREEN}Enter number to search for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
-        contact_search = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
-        
-        if contact_search in contact_dict:
+        print(f"{Fore.GREEN}There's no contacts to search.{Style.RESET_ALL}")
+        print("-" * 50)
+    else:
+        try:
             clr()
             print("-" * 50)
-            contact_details = contact_dict[contact_search]
-            print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {contact_details['Title']}")
-            print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
-            print(f"{Fore.GREEN}Phone Number: {Style.RESET_ALL}{contact_search}")
-            print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
-            print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
-            print("-" * 50)
-        else:
-            clr()
-            print("-" * 50)
-            print(f"{Fore.GREEN}The number:{Style.RESET_ALL} {contact_search}. {Fore.GREEN}Does not exist. Try again.{Style.RESET_ALL}")
+            print(f"{Fore.GREEN}Enter number to search for ex: xxx-xxx-xxxx{Style.RESET_ALL}")
+            contact_search = input(f"{Fore.GREEN}Enter Number:{Style.RESET_ALL} ")
+            
+            if contact_search in contact_dict:
+                clr()
+                print("-" * 50)
+                contact_details = contact_dict[contact_search]
+                print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {contact_details['Title']}")
+                print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
+                print(f"{Fore.GREEN}Phone Number: {Style.RESET_ALL}{contact_search}")
+                print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
+                print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
+                print("-" * 50)
+            else:
+                clr()
+                print("-" * 50)
+                print(f"{Fore.GREEN}The number:{Style.RESET_ALL} {contact_search}. {Fore.GREEN}Does not exist. Try again.{Style.RESET_ALL}")
+                print("-" * 50)
 
-    except ValueError:
-        clr()
-        print("-" * 50)
-        print(f"{Fore.RED}Error:{Style.RESET_ALL} Incorrect input")
+        except ValueError:
+            clr()
+            print("-" * 50)
+            print(f"{Fore.RED}Error:{Style.RESET_ALL} Incorrect input")
 
 def display_contacts():
     '''Displays all contacts in dictionary.'''
-    clr()
-    print("-" * 50)
-    for index, (contact_num, contact_details) in enumerate(contact_dict.items()):
-        print(f"{Fore.GREEN}{index + 1} - Title:{Style.RESET_ALL} {contact_details['Title']}")
-        print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
-        print(f"{Fore.GREEN}Phone Number:{Style.RESET_ALL} {contact_num}")
-        print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
-        print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
+    if len(contact_dict) == 0:
+        clr()
         print("-" * 50)
+        print(f"{Fore.GREEN}There's no contacts to display.{Style.RESET_ALL}")
+        print("-" * 50)
+    else:
+        clr()
+        print("-" * 50)
+        if len(contact_dict) == 0:
+            print(f"{Fore.RED}Contact dictionary is empty.{Style.RESET_ALL}\n")
+        for index, (contact_num, contact_details) in enumerate(contact_dict.items()):
+            print(f"{Fore.GREEN}Contact - {index + 1}{Style.RESET_ALL}")
+            print(f"{Fore.GREEN}Title:{Style.RESET_ALL} {contact_details['Title']}")
+            print(f"{Fore.GREEN}Name:{Style.RESET_ALL} {contact_details['Name']}")
+            print(f"{Fore.GREEN}Phone Number:{Style.RESET_ALL} {contact_num}")
+            print(f"{Fore.GREEN}Email:{Style.RESET_ALL} {contact_details['Email']}")
+            print(f"{Fore.GREEN}Notes:{Style.RESET_ALL} {contact_details['Notes']}")
+            print("-" * 50)
 
 def export_contacts():
-    with open('contact_file.txt', 'w') as file:
-        for contact, details in contact_dict.items():
-            file.write(f"{contact}: Name: {details.get('Name', 'N/A')},"
-                       f"Phone Number: {details.get('Phone Number', 'N/A')},"
-                       f"Email: {details.get('Email', 'N/A')},"
-                       f"Notes: {details.get('Notes', 'N/A')}\n")
+    '''Exports all contacts to a txt file.'''
+    if len(contact_dict) == 0:
+        clr()
+        print("-" * 50)
+        print(f"{Fore.GREEN}There's no contacts to export.{Style.RESET_ALL}")
+        print("-" * 50)
+    else:
+        print("The following contacts have been exported:")
+        display_contacts()
+        with open('contact_file.txt', 'w') as file:
+            for contact, details in contact_dict.items():
+                file.write(f"{contact}: Name: {details.get('Name', 'N/A')},"
+                        f"Title: {details.get('Title', 'N/A')},"
+                        f"Email: {details.get('Email', 'N/A')},"
+                        f"Notes: {details.get('Notes', 'N/A')}\n")
 
 def import_contacts():
-    pass
+    '''Takes text file and adds contacts to contact dictionary.'''
+    file_path = 'contact_file.txt'
+    
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            print("-" * 50)
+            print(f"{Fore.GREEN}Nothing to append.{Style.RESET_ALL}")
+            print("-" * 50)
+        return
+    
+    if os.stat(file_path).st_size == 0:
+        print("-" * 50)
+        print(f"{Fore.GREEN}Nothing to append.{Style.RESET_ALL}")
+        print("-" * 50)
+        return 
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            if not line.strip():
+                continue
+
+            contact, details = line.strip().split(': ', 1)
+            details_parts = details.split(',')
+            details_dict = {}
+
+            for part in details_parts:
+                key, value = part.split(': ', 1)
+                details_dict[key] = value
+
+            contact_dict[contact] = details_dict
+    
+    print(f"{Fore.GREEN}All contacts have been imported.{Style.RESET_ALL}")
+    print("-" * 50)
 
 def run_program():
     '''Runs Menu Options. Serves as the Main Program.'''
@@ -232,8 +304,8 @@ def run_program():
         print(f"{Fore.GREEN}3.{Style.RESET_ALL} Delete a contact")
         print(f"{Fore.GREEN}4.{Style.RESET_ALL} Search for a contact")
         print(f"{Fore.GREEN}5.{Style.RESET_ALL} Display all contacts")
-        print(f"{Fore.GREEN}6.{Style.RESET_ALL} Export contacts to a text file")
-        print(f"{Fore.GREEN}7.{Style.RESET_ALL} Import contacts from a text file *BONUS*")
+        print(f"{Fore.GREEN}6.{Style.RESET_ALL} Export contacts")
+        print(f"{Fore.GREEN}7.{Style.RESET_ALL} Import contacts")
         print(f"{Fore.GREEN}8.{Style.RESET_ALL} Quit")
         option = int(input(f"{Fore.GREEN}Enter Option Number: {Style.RESET_ALL}"))
 
